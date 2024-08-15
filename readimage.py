c364bfea4183e0e62cb0a1ab5d981118775ec508
Namitak _23 <159888750+Namitakafare@users.noreply.github.com>
2024-08-15 12:25:42 +0530

    Add files via upload

---
+++ readimage.py
@@ -0,0 +1,20 @@
+import cv2
+import numpy as np
+
+numberPlateCascade = cv2.CascadeClassifier('haarcascade_russian_plate_number.xml') 
+plat_detector =  cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_russian_plate_number.xml")
+img = cv2.imread('img (1).jpg')
+#gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+plates = plat_detector.detectMultiScale(img,scaleFactor=1.2,
+    minNeighbors = 5, minSize=(25,25))   
+
+for (x,y,w,h) in plates:
+    cv2.putText(img,text='License Plate',org=(x-3,y-3),fontFace=cv2.FONT_HERSHEY_COMPLEX,color=(0,0,255),thickness=1,fontScale=0.6)
+    img[y:y+h,x:x+w] = cv2.blur(img[y:y+h,x:x+w],ksize=(10,10))
+    cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+  
+
+cv2.imshow('plates',img)
+if cv2.waitKey(0) & 0xFF == ord('q'):
+    cv2.destroyAllWindows()
---
+++ readvideo.py
@@ -0,0 +1,31 @@
+import cv2
+import numpy as np
+
+#numberPlateCascade = cv2.CascadeClassifier('haarcascade_russian_plate_number.xml')
+plat_detector =  cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_russian_plate_number.xml")
+video = cv2.VideoCapture('vid.mp4')
+
+if(video.isOpened()==False):
+    print('Error Reading Video')
+
+while True:
+    ret,frame = video.read()    
+    gray_video = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    plate = plat_detector.detectMultiScale(gray_video,scaleFactor=1.2,minNeighbors=5,minSize=(25,25))
+
+    for (x,y,w,h) in plate:
+        cv2.rectangle(frame, (x,y), (x+w,y+h), (255,0,0),2)
+        frame[y:y+h,x:x+w] = cv2.blur(frame[y:y+h,x:x+w],ksize=(10,10))
+        cv2.putText(frame,text='License Plate',org=(x-3,y-3),fontFace=cv2.FONT_HERSHEY_COMPLEX,color=(0,0,255),thickness=1,fontScale=0.6)
+        
+         
+    if ret == True:
+        cv2.imshow('Video', frame)
+
+        if cv2.waitKey(25) & 0xFF == ord("q"):
+            break
+    else:
+        break
+
+video.release()
+cv2.destroyAllWindows()            
